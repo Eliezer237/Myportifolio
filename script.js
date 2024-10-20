4e9282e53e90927b35e5b13a6bd53c410f593117
Eliezer Niyorukundo <138326048+Bigdanton@users.noreply.github.com>
2024-10-21 01:05:33 +0200

    added lines of code

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showSidebar(){
+    const Sidebar=document.querySelector(".Sidebar")
+    Sidebar.style.display="flex"
+}
+function hideSidebar(){
+  const Sidebar=document.querySelector(".Sidebar")
+    Sidebar.style.display="none"
+} 
