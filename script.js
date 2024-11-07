45ec2e23030a596e0701f57c28002b5e0449c696
Eliezer Niyorukundo <138326048+Eliezer237@users.noreply.github.com>
2024-11-07 15:22:24 +0100

    Uploaded files

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 
-function showSidebar(){
-    const Sidebar=document.querySelector(".Sidebar")
-    Sidebar.style.display="flex"
-}
-function hideSidebar(){
-  const Sidebar=document.querySelector(".Sidebar")
-    Sidebar.style.display="none"
-} 
+
+
+
+
